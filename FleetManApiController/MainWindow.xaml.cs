4b8ca6b56e00ecf05b7308f94e98604a8f76ad73
Andre Jesus <andre.silva@verisys.com.br>
2020-08-31 09:56:23 -0300

    Criação do controle de usuário para mostrar status dos serviços

--- FleetManApiController/MainWindow.xaml.cs
+++ FleetManApiController/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace FleetManApiController
         public MainWindow()
         {
             InitializeComponent();
+            ServiceStatusUC.ChangeStatus(true);
+            DBConnectionStatusUC.ChangeStatus(false);
         }
     }
 }
---
+++ FleetManApiController/UserControls/StatusLabelUC.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FleetManApiController.UserControls
+{
+    /// <summary>
+    /// Interação lógica para StatusLabelUC.xam
+    /// </summary>
+    public partial class StatusLabelUC : UserControl, INotifyPropertyChanged
+    {
+        private Color _greencolor = Color.FromArgb(255, 55, 255, 35);
+        private Color _redcolor = Color.FromArgb(255, 255, 30, 30);
+
+        public string Description { get; set; }
+
+        public StatusLabelUC()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public async Task ChangeStatus(bool status)
+        {
+            await Dispatcher.InvokeAsync(() =>
+            {
+                if (status)
+                {
+                    StateLabel.Content = "Ativo";
+                    StatusEllipse.Fill = new SolidColorBrush(_greencolor);
+                }
+                else
+                {
+                    StateLabel.Content = "Inativo";
+                    StatusEllipse.Fill = new SolidColorBrush(_redcolor);
+                }
+            });
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string caller = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
+        }
+    }
+}
