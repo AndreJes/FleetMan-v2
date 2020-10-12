dda33ccd815fcbf3cd01831f193f7ab330250096
AndreJes <andrej.silva@outlook.com>
2020-10-12 10:39:51 -0300

    Serviço de segurança e loginData com Salt

--- FleetManModel/Classes/LoginData.cs
+++ FleetManModel/Classes/LoginData.cs
@@ -14,5 +14,6 @@ namespace FleetManModel.Classes
         public string Email { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public string Salt { get; set; }
     }
 }
---
+++ FleetManServices/Security/FleetManHash.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetManServices.Security
+{
+    public class FleetManHash
+    {
+        private byte[] GetHash(string saltedpassword)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+            {
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(saltedpassword));
+            }
+        }
+
+        public string GetSalt()
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            byte[] buffer = new byte[512];
+
+            provider.GetBytes(buffer);
+            string salt = BitConverter.ToString(buffer);
+            return salt.Replace("-", "");
+        }
+
+        public string GetHashedPassword(string password, string salt)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(password + salt))
+            {
+                sb.Append(b.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
